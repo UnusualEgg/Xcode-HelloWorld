eeb4ea4283f898729ba0f87d3d34e381649a7b44
UnusualEgg <76134596+UnusualEgg@users.noreply.github.com>
2022-02-16 08:08:57 -0600

    Add files via upload

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Lentz, Jade on 2/16/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // urmum
+    char buffer[10];
+    std::cin >> buffer;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
